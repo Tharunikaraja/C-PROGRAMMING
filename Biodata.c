5ed02c0e997f82ddf2915d1ea63df7a9cbdf2496
Tharunika <tharunikaraja333@gmail.com>
2024-09-20 06:42:48 +0530

    Add files via upload

---
+++ Biodata.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name                      : Tharunika R\n");
+    printf("Gender                    : Female\n");
+    printf("Father's Name             : Raja SR\n");
+    printf("mother's Name             : Sudha M\n");
+    printf("Blood Group               : A-ve\n");
+    printf("Date of Birth             : 19.02.2007\n");
+    printf("Address                   : 6/21,Ayakadu,Mannuvakattupalayam,\n");
+    printf("                            Mallasamudram(west),Vadugampalayam(p.o),\n");
+    printf("                            Tiruchengode(T.k),Namakkal(D.t)\n ");
+    printf("State                     : Tamil Nadu\n");
+    printf("Nationality               : Indian\n");
+    printf("Religion                  : Hindhu\n");
+    printf("Father's Occupation       : Farmer\n");
+    printf("mother's Occupation       : Home maker\n");
+    printf("Educational Qualification : 12th grade\n");
+    printf("Email Address             : tharunikaraja333@gmail.com\n");
+    printf("Mobile no                 : 9952393357\n");
+    printf("Hobbies                   : Reading books,Cooking,Gardening\n");
+    printf("Languages Known           : Tamil,English\n");
+    printf("Strength                  : postive attitude towards work\n");
+    
+    
+    return 0;
+}
