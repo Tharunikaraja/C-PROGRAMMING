e87522f4afe26ce36fff3e3b4fdb6165bb33bf4f
Tharunika <tharunikaraja333@gmail.com>
2024-09-22 12:25:20 +0530

    Update Biodata.c

--- Biodata.c
+++ Biodata.c
@@ -8,6 +8,7 @@ int main()
     printf("mother's Name             : Sudha M\n");
     printf("Blood Group               : A-ve\n");
     printf("Date of Birth             : 19.02.2007\n");
+    printf("Age                       : 18\n");
     printf("Address                   : 6/21,Ayakadu,Mannuvakattupalayam,\n");
     printf("                            Mallasamudram(west),Vadugampalayam(p.o),\n");
     printf("                            Tiruchengode(T.k),Namakkal(D.t)\n ");
