69b74f6e07e863a708f9212a41d6b66a86b0b6dd
Tharunika <tharunikaraja333@gmail.com>
2024-09-22 12:23:44 +0530

    Add files via upload

---
+++ printing hello world using c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+printf(" c     c  cccccc c       c      ccccccc    c          c  ccccccc  cccccc c      cccccccc  \n");
+printf(" c     c  c      c       c      c     c    c    c     c  c     c  c    c c      c       c \n");
+printf(" c     c  c      c       c      c     c    c    cc    c  c     c  c    c c      c        c\n");
+printf(" ccccccc  cccccc c       c      c     c    c   c  c   c  c     c  cccccc c      c        c\n");
+printf(" c     c  c      c       c      c     c    c  c    c  c  c     c  c c    c      c       c \n");
+printf(" c     c  c      c       c      c     c    c c      c c  c     c  c  c   c      c      c  \n");
+printf(" c     c  cccccc ccccccc cccccc ccccccc    c          c  ccccccc  c   c  cccccc ccccccc   \n");
+
+}
